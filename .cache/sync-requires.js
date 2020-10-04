60d79ad54c062b6bff21613000d31963da725da3
Calista Nguyen <nguyen.calista@gmail.com>
2020-10-04 16:06:39 -0600

    setting up environment

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/calistanguyen/git/climb-on/.cache/dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/calistanguyen/git/climb-on/src/pages/index.js")))
+}
+
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Home = () => {
+  return <div>Hello CSCI3010!</div>
+}
+
+export default Home; 
