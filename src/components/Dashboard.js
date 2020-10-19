dad19a5d6411b2c6431bba2437ccf6ec2e860976
Calista Nguyen <nguyen.calista@gmail.com>
2020-10-19 16:20:20 -0600

    adding nav state

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+const Dashboard = () =>{
+    return(
+        <div className = "dashboard">
+            HAHA
+            
+        </div>
+    ); 
+}
+
+export default Dashboard
