6f92ab12fd03799dfabdde70858fca37091fc4db
Calista Nguyen <nguyen.calista@gmail.com>
2020-10-18 21:39:22 -0600

    adding nav bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,20 @@
 import React from "react"
+import NavBar from '../components/NavBar'
 
-const Home = () => {
-  return <div>Hello CSCI3010!</div>
+import '../styles/global.scss'
+// import 'antd/dist/antd.css';
+// import NavBar from  '../components/NavBar'
+// import { Layout } from 'antd';
+// const { Header, Footer, Sider, Content } = Layout;
+
+const App = () => {
+  return (
+    <div>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;800&display=swap" rel="stylesheet"/>
+      <NavBar/>
+      
+    </div>
+  );
 }
 
-export default Home; 
+export default App; 
