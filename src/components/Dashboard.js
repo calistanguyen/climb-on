6d2aed28ec8df1bbd6ea64c8581d0cac74a1385d
Calista Nguyen <nguyen.calista@gmail.com>
2020-10-23 16:40:27 -0600

    added experimental card to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,12 @@
-import React from 'react'; 
+import React from 'react';
+import LogCard from './LogCard';
 
-const Dashboard = () =>{
-    return(
-        <div className = "dashboard">
-            HAHA
-            
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <LogCard />
         </div>
-    ); 
+    );
 }
 
 export default Dashboard
