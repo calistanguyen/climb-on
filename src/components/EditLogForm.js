1ed195c57f374d09abf9e3a26ae3e246fc88a8d6
Calista Nguyen <nguyen.calista@gmail.com>
2020-11-17 20:56:25 -0700

    removing test.js

--- src/components/EditLogForm.js
+++ src/components/EditLogForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { getUser } from '../services/auth'
 import { gql, request } from 'graphql-request'
 
