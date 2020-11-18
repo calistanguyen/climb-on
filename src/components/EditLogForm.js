74de75a46152eb58e52c15a891568d00c2d7b01f
Calista Nguyen <nguyen.calista@gmail.com>
2020-11-17 20:49:48 -0700

    finishing up edit climb log form without adding img

--- src/components/EditLogForm.js
+++ src/components/EditLogForm.js
@@ -1,8 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { getUser } from '../services/auth'
+import { gql, request } from 'graphql-request'
 
-const tries = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const triesArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const topLeadLevels = ["5.5", "5.6", "5.7", "5.8", "5.9", "5.10", "5.11", "5.12", "5.13"]
 const boulderLevels = ["v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9"]
+
+async function mutation(userId, type, level, userNotes, tries, completed, date) {
+    const mutation = gql`
+    mutation createClimb($input: CreateClimbInput!){
+        createClimb(input: $input){
+          climb{
+            userId, 
+            imgLink, 
+            type, 
+            level, 
+            userNotes, 
+            tries, 
+            completed, 
+            date
+          }
+        }
+      }
+  `
+    const variables = {
+        input: {
+            climb: {
+                userId: userId,
+                type: type,
+                level: level,
+                userNotes: userNotes,
+                tries: Number(tries),
+                completed: Boolean(completed),
+                date: date
+            }
+        }
+    }
+
+    await request('http://localhost:5000/graphql', mutation, variables).then((data) => {
+        console.log('added to data base')
+    },
+        error => {
+            console.log(error)
+        })
+}
 
 function setToday() {
     var today = new Date()
@@ -23,9 +64,14 @@ function setToday() {
 }
 const today = setToday();
 const EditLogForm = () => {
-    const [topLead, setTopLead] = useState(false);
-    const [boulder, setBoulder] = useState(false);
-
+    const [topLead, setTopLead] = useState(false)
+    const [boulder, setBoulder] = useState(false)
+    const [type, setType] = useState('')
+    const [level, setLevel] = useState('')
+    const [tries, setTries] = useState(0)
+    const [completed, setCompleted] = useState(false)
+    const [date, setDate] = useState('')
+    const [notes, setNotes] = useState('')
     function setLevelTopLead() {
         setTopLead(true)
         setBoulder(false)
@@ -34,25 +80,66 @@ const EditLogForm = () => {
         setBoulder(true)
         setTopLead(false)
     }
+
+    const handleType = event => {
+        setType(event.target.value);
+    }
+    const handleLevel = event => {
+        setLevel(event.target.value);
+    }
+    const handleTries = event => {
+        setTries(event.target.value)
+    }
+    const handleCompleted = event => {
+        setCompleted(event.target.value)
+    }
+
+    const handleNotes = event => {
+        setNotes(event.target.value)
+    }
+
+    const handleDate = event => {
+        setDate(event.target.value)
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log('---type---', type)
+        console.log('---level---', level)
+        console.log('---tries---', tries)
+        console.log('---completed---', completed)
+        console.log('---date---', date)
+        console.log('---notes---', notes)
+    }
+
+
     return (
         <div className="edit-log">
             <div className="header" >Add to Log</div>
-            <form>
+            <form amethod="post"
+                onSubmit={event => {
+                    handleSubmit(event)
+                    mutation(getUser().id, type, level, notes, tries, completed, date).then(auth => {
+                        console.log('added climb')
+                    }).catch(err => {
+                        console.log(err)
+                    })
+                }}>
                 <div className="type">
                     <div className="type-label">Type: </div>
-                    <input type="radio" id="boulder" name="type" value="Boulder" onClick={setLevelBoulder} />
+                    <input type="radio" id="type" name="type" value="Boulder" onClick={setLevelBoulder} onChange={handleType} />
                     <label htmlFor="boulder">Boulder</label>
-                    <input type="radio" id="top-rope" name="type" value="Top Rope" onClick={setLevelTopLead} />
+                    <input type="radio" id="type" name="type" value="Top Rope" onClick={setLevelTopLead} onChange={handleType} />
                     <label htmlFor="top-rope">Top Rope</label>
-                    <input type="radio" id="lead" name="type" value="Lead" onClick={setLevelTopLead} />
+                    <input type="radio" id="type" name="type" value="Lead" onClick={setLevelTopLead} onChange={handleType} />
                     <label htmlFor="lead">Lead</label>
                 </div>
 
                 <div className="level-and-tries">
                     <div className="level">
-
                         <div className="level-label">Level:</div>
-                        <select id="level" name="level">
+                        <select id="level" name="level" onChange={handleLevel}>
+                            <option />
                             {
                                 topLead &&
                                 topLeadLevels.map((item) =>
@@ -68,8 +155,9 @@ const EditLogForm = () => {
 
                     <div className="tries">
                         <div className="tries-label">Tries:</div>
-                        <select id="tries" name="tries">
-                            {tries.map((item) =>
+                        <select id="tries" name="tries" onChange={handleTries}>
+                            <option />
+                            {triesArr.map((item) =>
                                 <option value={item}>{item}</option>
                             )}
                         </select>
@@ -78,20 +166,21 @@ const EditLogForm = () => {
                 <div className="completed-and-date">
                     <div className="completed">
                         <div className="completed-label">Completed:</div>
-                        <select id="tries" name="tries">
+                        <select id="tries" name="tries" onChange={handleCompleted}>
+                            <option />
                             <option value={true}>Yes</option>
                             <option value={false}>No</option>
                         </select>
                     </div>
                     <div className="date">
                         <div className="date-label">Date: </div>
-                        <input type="date" id="date" name="date" defaultValue={today} />
+                        <input type="date" id="date" name="date" defaultValue={today} onChange={handleDate} />
                     </div>
                 </div>
 
                 <div className="user-notes">
                     <div className="notes-label">Notes on the Climb:</div>
-                    <textarea name="user_notes" rows="10" cols="30" defaultValue="Add notes about the climb here" />
+                    <textarea name="user_notes" rows="10" cols="30" defaultValue="Add notes about the climb here" onChange={handleNotes} />
                 </div>
 
                 <input type="submit" value="Submit" />
